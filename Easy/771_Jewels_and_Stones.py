3c9dc50711f9a4a6be2c2b43c327c5f19b5426b8
Harsh Gupta <harshgupta2204@live.com>
2019-01-26 20:03:04 -0500

    Initial Upload

---
+++ Easy/771_Jewels_and_Stones.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        counter = 0
+        for jewel in J:
+            counter += S.count(jewel)
+        return counter
